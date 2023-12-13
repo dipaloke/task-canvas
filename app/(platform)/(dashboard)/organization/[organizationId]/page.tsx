5e4a64523984430ffcc15dddc8ffb961fa81a9a8
dipaloke <dipalokebiswas96@gmail.com>
2023-12-13 14:37:14 +0600

    How to get organization details and how organizationSwitcher component from clerk works

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,13 @@
+import { OrganizationSwitcher, auth } from "@clerk/nextjs";
+import React from "react";
+
+const OrganizationIdPage = () => {
+  const { userId, orgId } = auth();
+  return (
+    <div>
+      <OrganizationSwitcher hidePersonal />
+    </div>
+  );
+};
+
+export default OrganizationIdPage;
