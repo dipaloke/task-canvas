e5f99d924036b9128656fa018b709f60ec4cb02c
dipaloke <dipalokebiswas96@gmail.com>
2023-12-13 19:12:41 +0600

    Created dashboard navbar, Next improve middleware

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,13 +1,7 @@
-import { OrganizationSwitcher, auth } from "@clerk/nextjs";
 import React from "react";
 
 const OrganizationIdPage = () => {
-  const { userId, orgId } = auth();
-  return (
-    <div>
-      <OrganizationSwitcher hidePersonal />
-    </div>
-  );
+  return <div>Organization ID page</div>;
 };
 
 export default OrganizationIdPage;
