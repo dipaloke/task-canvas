05147022e9b619392e79c52aebadfcf53508a92a
dipaloke <dipalokebiswas96@gmail.com>
2023-12-18 12:23:05 +0600

    Error handling. How to Get error form server componet and show them to client. Next loading stste

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -3,24 +3,52 @@
 import { z } from "zod";
 
 import { db } from "@/lib/db";
-import { title } from "process";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export type State = {
+  errors?: {
+    title?: string[];
+  };
+  message?: string | null;
+};
 
 //schema: We will be creating an obj with a title field as string
 const CreateBoard = z.object({
-  title: z.string(),
+  title: z.string().min(3, {
+    message: "Minimum length of 3 letters is required",
+  }),
 });
 
-export async function create(formData: FormData) {
-  const { title } = CreateBoard.parse({
+//server action which will be used in Form client component.
+
+export async function create(prevState: State, formData: FormData) {
+  const validatedFields = CreateBoard.safeParse({
     title: formData.get("title"),
   });
 
-  await db.board.create({
-    data: {
-      title,
-    },
-  });
+  //checking for errors
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields.",
+    };
+  }
+
+  const { title } = validatedFields.data;
+
+  try {
+    await db.board.create({
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+      message: "Database Error",
+    };
+  }
 
   revalidatePath("/organization/org_2ZUeDuu2IJmRktGYSJ2VFIPe9Wx");
+  redirect("/organization/org_2ZUeDuu2IJmRktGYSJ2VFIPe9Wx");
 }
