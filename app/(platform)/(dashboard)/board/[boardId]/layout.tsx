edd4d700be2ad0d752955f09f58f69d32e871161
dipaloke <dipalokebiswas96@gmail.com>
2023-12-24 20:20:43 +0600

    Finished Fetching current board

--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 
-const BoardIdLayout = ({
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+
+import { notFound, redirect } from "next/navigation";
+
+const BoardIdLayout = async ({
   children,
   params,
 }: {
   children: React.ReactNode;
   params: { boardId: string };
 }) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+  //fetching current board
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  //redirects to 404 not found page
+  if (!board) {
+    notFound();
+  }
   return (
     <main className="relative pt-20 h-full">
       <div>{children}</div>;
