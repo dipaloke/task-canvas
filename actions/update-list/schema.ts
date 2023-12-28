4c104136984af4b6a9c1f51e0e330710fee4b6a1
dipaloke <dipalokebiswas96@gmail.com>
2023-12-28 21:54:05 +0600

    Rendering newe list & card: (created listItem component, listHeader componrnt), Functionality:(header is a form, can be edited by clicking on it, then pressing enter or scape or by clicking outside of the header field) Next: (options to copy/delete list)

---
+++ actions/update-list/schema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const UpdateList = z.object({
+  title: z
+    .string({
+      required_error: "Title is required",
+      invalid_type_error: "Title is required",
+    })
+    .min(3, {
+      message: "Title is too short",
+    }),
+  id: z.string(), // BY using Id we will know which board user want to update
+  boardId: z.string(),
+});
--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -3,18 +3,30 @@
 import { ListWithCards } from "@/types";
 
 import { ListForm } from "./list-form";
+import { useEffect, useState } from "react";
+import { ListItem } from "./list-item";
 
 interface ListContainerProps {
   data: ListWithCards[];
   boardId: string;
 }
 
-export const ListContainer = ({data, boardId}: ListContainerProps) => {
-    return (
-        <ol>
-            <ListForm />
-            {/* represents the empty space at the end of the x axis */}
-            <div className="flex-shrink-0 w-1" />
-        </ol>
-    )
-}
+export const ListContainer = ({ data, boardId }: ListContainerProps) => {
+  //storing the lists locally to add drag and drop feature.(we first change locally then the database)
+  const [orderedData, setOrderedData] = useState(data);
+
+  useEffect(() => {
+    setOrderedData(data);
+  }, [data]);
+
+  return (
+    <ol className="flex gap-x-3 h-full">
+      {orderedData.map((list, index) => {
+        return <ListItem key={list.id} index={index} data={list} />;
+      })}
+      <ListForm />
+      {/* represents the empty space at the end of the x axis */}
+      <div className="flex-shrink-0 w-1" />
+    </ol>
+  );
+};
