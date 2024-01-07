218ad79ff4b34a95afe4ee728d472fdf0b0d99ed
dipaloke <dipalokebiswas96@gmail.com>
2024-01-07 22:34:50 +0600

    CLICKING SINGLE CARD SHOWS CARD INFO (modal): (Description Component : Create Skeleton & render conditionally, Create description component, Added Editing functionality (Refs and EnableEditing func), conditionally rendering Description data or Editing State, Reuse the updateCard serverAction to submit.)(NEXT: Actions and Action skiliton)

--- actions/update-card/schema.ts
+++ actions/update-card/schema.ts
@@ -1,20 +1,26 @@
 import { z } from "zod";
 
 export const UpdateCard = z.object({
-    boardId: z.string(),
-    description: z.optional(
-        z.string({
-            required_error: "Description is required",
-            invalid_type_error: "Description is required"
-        }).min(3, {
-            message: "Description is too short."
-        })
-    ),
-    title: z.string({
+  boardId: z.string(),
+  description: z.optional(
+    z
+      .string({
+        required_error: "Description is required",
+        invalid_type_error: "Description is required",
+      })
+      .min(3, {
+        message: "Description is too short.",
+      })
+  ),
+  title: z.optional(
+    z
+      .string({
         required_error: "Title is required",
         invalid_type_error: "Title is required",
-    }).min(3, {
-        message: "Title is too short"
-    }),
-    id: z.string(), // BY using Id we will know which board user want to update
-})
+      })
+      .min(3, {
+        message: "Title is too short",
+      })
+  ),
+  id: z.string(), // BY using Id we will know which board user want to update
+});
