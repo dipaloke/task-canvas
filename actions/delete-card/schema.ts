10b9d96b670f243d47ad471b47bce6c1ff22fad1
dipaloke <dipalokebiswas96@gmail.com>
2024-01-08 22:32:21 +0600

    CLICKING SINGLE CARD SHOWS CARD INFO (modal): (Actions Component: Created the actions skeletonand conditionally rendered skeleton or actions component, (Created actions component : Copy & delete btn, created new varient and new size for ShadCn btn), Server Actions for the buttons, (Intregrated in Actions component : modal should be closed after copy or delete). (NEXT: Activity log Tab))

---
+++ actions/delete-card/schema.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const DeleteCard = z.object({
+  id: z.string(), // BY using Id we will know which board user want to delete
+  boardId: z.string(),
+});
--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -10,6 +10,7 @@ import { fetcher } from "@/lib/fetcher";
 
 import { Header } from "./header";
 import { Description } from "./description";
+import { Actions } from "./actions";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
@@ -28,9 +29,14 @@ export const CardModal = () => {
         <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
           <div className="col-span-3">
             <div className="w-full space-y-6">
-              {!cardData ? <Description.Skeleton /> : <Description data={cardData} />}
+              {!cardData ? (
+                <Description.Skeleton />
+              ) : (
+                <Description data={cardData} />
+              )}
             </div>
           </div>
+          {!cardData ? <Actions.Skeleton /> : <Actions data={cardData} />}
         </div>
       </DialogContent>
     </Dialog>
